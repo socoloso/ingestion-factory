cd97428cd47094bf34ab77bc3fb06208bfa8dc61
socoloso <tafsteve@gmail.com>
2016-02-20 11:04:52 -0800

    learned the value of a for loop
    ability modifiers are now created with a function and a for loop

--- characterSheet/characterSheetBasics.js
+++ characterSheet/characterSheetBasics.js
@@ -1,23 +1,14 @@
-// Characters base attributes, in the future these will not be hardcoded
-var str = 18;
-var dex = 14;
-var con = 14;
-var int = 8;
-var wis = 10;
-var cha = 8;
+// In the future these will not be hardcoded
+var abilityScores = [18, 14, 14, 8, 12, 8];
+var skills = [];
+var abilityMods = [];
 
-// This is the math for how ability modifiers are generated from ability scores in D&D systems 
-var abilityMod = function (abilityScore) {
-	return ((abilityScore - 10) / 2 - 0.5).toFixed(0);
+var scoreMath = function (abilityScores) {
+	return ((abilityScores - 10) / 2 - 0.5).toFixed(0);
 }
 
-// These values are used for most important stats such as skills, spells, attacks, etc.
-var strMod = abilityMod(str);
-var dexMod = abilityMod(dex);
-var conMod = abilityMod(con);
-var intMod = abilityMod(int);
-var wisMod = abilityMod(wis);
-var chaMod = abilityMod(cha);
+for (var i = 0; i < abilityScores.length; i++) {
+	 abilityMods[i] = scoreMath(abilityScores[i]);
+};
 
-// This is just a test to make sure that my math worked
-console.log(strMod, dexMod, conMod, intMod, wisMod, chaMod);
+console.log(abilityMods);
